61b8625f249ddc5ea35468a3ebbb7f8b7be6fe8d
manu <clivaz@mac.com>
2019-12-23 09:34:44 +0100

    Added Comic Book Detail view

--- src/ComicBookGallery2/ComicBookGallery2/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery2/ComicBookGallery2/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace ComicBookGallery2.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");        
+            return View();   
         }
     }
 }
