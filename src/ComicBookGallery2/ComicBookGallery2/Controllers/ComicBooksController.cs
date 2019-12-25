211b92c570613480912905614cc03a84811bd385
manu <clivaz@mac.com>
2019-12-25 11:52:08 +0100

    Added comic book repository

--- src/ComicBookGallery2/ComicBookGallery2/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery2/ComicBookGallery2/Controllers/ComicBooksController.cs
@@ -11,25 +11,9 @@ namespace ComicBookGallery2.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                
-                // Start by instantiating the array
-                Artists = new Artist[]
-                { 
-                    // Then add an artist model instance with name + role properties
-                    new Artist() {Name = "Dan Slott", Role = "Script"},
-                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-            };
+
             // Pass our comic books instances to our view
-            return View(comicBook);   
+            return View();   
         }
     }
 }
